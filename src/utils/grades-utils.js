040371ac1ee345beba4533bf91bfd1155495fdb8
David Aguilar <davidaguilarh.94@gmail.com>
2019-07-19 00:13:52 -0500

    Set code for average test

--- src/utils/grades-utils.js
+++ src/utils/grades-utils.js
@@ -1,8 +1,1 @@
-export const getAverage = (array) => {
-    if (Array.isArray(array)) {
-        const sum = array.reduce((accum, val) => accum + parseInt(val, 10) || 0, 0);
-        const numAmount = array.length;
-        return sum / numAmount;
-    }
-    return null;
-};
+export const getAverage = () => {};
